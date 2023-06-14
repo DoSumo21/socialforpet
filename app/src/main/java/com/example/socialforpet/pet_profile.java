c96939acbb7973c7e4e1e47bda74c62c324a94fb
DESKTOP-EP38VHI\ASUS <kiendang1288@gmail.com>
2023-06-14 23:23:15 +0700

    Chức nawg A

---
+++ app/src/main/java/com/example/socialforpet/pet_profile.java
@@ -0,0 +1,13 @@
+package com.example.socialforpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pet_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_profile);
+    }
+}
