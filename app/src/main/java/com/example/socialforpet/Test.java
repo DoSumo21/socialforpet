1370b8d6b799827fe99e24f5fb3a150cd8468da4
PC <hoduythanh210102@gmail.com>
2023-06-15 10:04:53 +0700

    commit

--- app/src/main/java/com/example/socialforpet/Test.java
+++ app/src/main/java/com/example/socialforpet/Test.java
@@ -2,13 +2,36 @@ package com.example.socialforpet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Test extends AppCompatActivity {
 
+    private Button btnCreate;
+    private Button btnClose;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        btnCreate = findViewById(R.id.btnCreate);
+        btnClose = findViewById(R.id.btnDeSau);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Test.this, Thong_tin_pet.class );
+                startActivity(intent);
+            }
+        });
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Test.this, MainActivity.class );
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/socialforpet/Thong_tin_pet.java
@@ -0,0 +1,13 @@
+package com.example.socialforpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Thong_tin_pet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thong_tin_pet);
+    }
+}
