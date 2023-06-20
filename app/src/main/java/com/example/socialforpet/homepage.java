e52cda8e4e3245960f51b7ed9624c564c01763ef
PC <hoduythanh210102@gmail.com>
2023-06-20 21:30:21 +0700

    ccomiit

--- app/src/main/java/com/example/socialforpet/homepage.java
+++ app/src/main/java/com/example/socialforpet/homepage.java
@@ -13,14 +13,5 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-
-        btnShare = findViewById(R.id.btn_share);
-        btnShare.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(homepage.this, CreatePost.class );
-                startActivity(intent);
-            }
-        });
     }
 }
